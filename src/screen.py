1d39bab9f2d17ccfe858acec5aab063207a51de4
Beni Truninger <zoopajr@gmail.com>
2014-12-03 23:39:13 +0100

    added screen

--- src/screen.py
+++ src/screen.py
@@ -4,5 +4,17 @@ class Screen(object):
         self.height = height
         self.clear()
 
+    def getWidth(self):
+        return self.width
+
+    def getHeight(self):
+        return self.height
+
+    def setPixel(self, x, y, on):
+        pass
+
+    def getPixel(self, x, y):
+        pass
+
     def clear(self):
         self.screen = [0] * self.width * self.height
